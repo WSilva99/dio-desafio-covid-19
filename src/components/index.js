d9390d5b0c99165296c0f8fa182274a7b97bac9c
Walmir Silva <walmirsilva305@gmail.com>
2022-06-11 23:17:14 -0300

    desafio-covid

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import { Button, Card, CardContent, Grid, MenuItem, Select, Typography } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
+
+export {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+  Skeleton,
+  Select,
+  MenuItem
+}
